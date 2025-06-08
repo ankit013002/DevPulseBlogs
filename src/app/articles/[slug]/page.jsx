4f6a4d958e42a5ea6af5bcb47bcbcbb167ad7b50
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-06-07 23:49:42 -0400

    Fixed issue with searching and querying articles. The issue is just that article cards now more of a user's information. Seein a lot of repeated code which should be handled in next branch.

--- src/app/articles/[slug]/page.jsx
+++ src/app/articles/[slug]/page.jsx
@@ -1,20 +1,48 @@
+"use server";
+
 import { getArticlesBySlug } from "@/action/getArticles";
 import React from "react";
 import ArticleCard from "@/components/articleCard";
 import Link from "next/link";
 import { getUserFromCookie } from "@/lib/getUser";
+import { getBase64Image } from "@/action/getBase64Image";
+import { getUserInformationById } from "@/action/getUserInformation";
 
 const page = async function ({ params }) {
   const { slug } = await params;
   const articles = await getArticlesBySlug(slug);
+  let serializedArticles = await Promise.all(
+    articles.map(async (article) => {
+      const coverImage = await getBase64Image(article.coverImage);
+      const user = await getUserInformationById(article.userId);
+      const profilePicture = await getBase64Image(user.profilePicture);
+      return {
+        title: article.title,
+        link: article.link,
+        author: article.author,
+        date: article.date,
+        tags: article.tags,
+        coverImage: coverImage,
+        description: article.description,
+        content: article.content,
+        updatedAt: article.updatedAt,
+        user: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          username: user.username,
+          profilePicture: profilePicture,
+        },
+      };
+    })
+  );
 
   return (
     <div className="mt-[1%] w-[100%] h-[100%]">
-      {articles?.map((article, index) => {
+      {serializedArticles?.map((article, index) => {
         return (
           <div className="width-[100%]" key={index}>
             <Link href={`/article/${article.link}`}>
-              <ArticleCard cardInfo={article} />
+              <ArticleCard cardInfo={article} user={article.user} />
             </Link>
           </div>
         );
